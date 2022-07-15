6987b45995cdb04509d4900bed828d7279f3b632
kkailiwang <kailiwang2018@gmail.com>
2022-07-15 15:44:52 -0500

    fix styles

--- components/layout.js
+++ components/layout.js
@@ -20,36 +20,6 @@ const Layout = (props) => (
       <a href="https://privy.io">Privy</a>
     </footer>
 
-    <style jsx global>{`
-      *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-      body {
-        margin: 0;
-        color: #333;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-          'Helvetica Neue', Arial, Noto Sans, sans-serif, 'Apple Color Emoji',
-          'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-      }
-      .container {
-        max-width: 42rem;
-        margin: 0 auto;
-        padding: 2rem 1.25rem;
-      }
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      footer a {
-        padding: 20px;
-      }
-    `}</style>
   </>
 )
 
